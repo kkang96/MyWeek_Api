1fd96cf10f933f7382b96de3ae65339b2ba83d05
gckhahn <gckhahn@gccorp.com>
2022-04-04 16:40:54 +0900

    mysql 세팅

---
+++ src/db/dbconnect.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql';
+import { config } from '../config/index.js';
+
+export default {
+  init: () => {
+    return mysql.createConnection(config);
+  },
+  connect: (conn) => {
+    conn.connect((err) => {
+      if (err) console.log('err', err);
+      else console.log('db connect');
+    })
+  }
+}
